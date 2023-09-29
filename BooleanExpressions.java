5c7babf7dcde6b620bb21672da25fd5c7e045fd8
26-AMoore <26amoore@derryfield.org>
2023-09-29 11:04:48 -0400

    idk i added something

---
+++ BooleanExpressions.java
@@ -0,0 +1,8 @@
+public class BooleanExpressions {
+    public static void main(String[] args){
+        //Evaluate to true or false, not a number
+        boolean flag = true;
+        System.out.println(flag);
+    }
+
+}
