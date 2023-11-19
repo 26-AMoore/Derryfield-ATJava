260e607bae9a2f92a1e092b9958dc76cbbb7f770
26-AMoore <26amoore@derryfield.org>
2023-11-19 10:13:24 -0500

    i forgot what i did

---
+++ Project-05/Part1.java
@@ -0,0 +1,12 @@
+public class Part1 {
+	public static void main(String[] args) {
+		String l = whoIsFirst("Hello", "Goodbye");
+	}
+	public static String whoIsFirst(String sec1, String sec2) {
+		System.out.println(sec1);
+		System.out.println(sec2);
+	}
+	public static int makeNum(String sec1, String sec2) {
+		
+	}
+}
