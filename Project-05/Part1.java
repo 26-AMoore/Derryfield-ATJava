89bf917f2077cf31e9a2f31eff2021579357843c
26-AMoore <26amoore@derryfield.org>
2023-12-10 11:50:56 -0500

    Finished Project-05, and it was pretty fun

--- Project-05/Part1.java
+++ Project-05/Part1.java
@@ -1,15 +1,62 @@
+import java.util.Scanner;
+
 public class Part1 {
 	public static void main(String[] args) {
 		String first;
-	}
-	public static int FirstNum(String sec1) {
-		int firstsec;
-		for (int i = 0; i < sec1.length(); i++) {
-			if (sec1.charAt(i) == '.') {
-				firstsec = Integer.valueOf(sec1.substring(0, i));
+		String second;
+		boolean found = false;
+		boolean isEqual = false;
+		String largerSection = "yeeeHaw";
+		Scanner in = new Scanner(System.in);
+		String firstPart;
+		String secondPart;
+		
+		System.out.println("What is the first section name?");
+		first = in.next();
+		System.out.println("What is the second section name?");
+		second = in.next();
+		firstPart = first;
+		secondPart = second;
+		while (!found) {
+			if (firstPart == "null" && secondPart == "null") {
+				System.out.println("They are the same");
+				isEqual = true;
+				break;
+			}
+			if (firstNum(firstPart) > firstNum(secondPart)) {
+				largerSection = first;
+				found = true;
+			} else if (firstNum(secondPart) > firstNum(firstPart)) {
+				largerSection = second;
+				found = true;
+			} else {
+				firstPart = getNext(firstPart);
+				secondPart = getNext(secondPart);
 			}
 		}
-		return firstsec;
+		if (!isEqual) {
+			System.out.println("The largest section is "+ largerSection);
+		}
+	}
+	public static int firstNum(String sec1) {
+		for (int i = 0; i < sec1.length(); i++) {
+			if (sec1.charAt(i) == '.') {
+				return Integer.valueOf(sec1.substring(0, i));
+			} else if (i == sec1.length()-1) {
+				return Integer.valueOf(sec1);
+			}
+		}
+		return 0;
+	}
+	public static String getNext(String sec) {
+		for (int i = 0; i < sec.length(); i++) {
+			if (sec.charAt(i) == '.') {
+				return sec.substring(i+1, sec.length());
+			} else if (i == sec.length()) {
+				return "same";
+			}
+		}
+		return "null";
 	}
 
 }
