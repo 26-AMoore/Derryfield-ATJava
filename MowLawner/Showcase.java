3173b650d68159d02ac836be24d99238e2e134ec
26-AMoore <26amoore@derryfield.org>
2024-02-19 10:01:04 -0500

    LawnMower

---
+++ MowLawner/Showcase.java
@@ -0,0 +1,13 @@
+import org.derryfield.Yard;
+public class Showcase {
+	public static void main(String[] args) {
+		Yard yard = new Yard(10, 10);
+		yard.showYard();
+		System.out.println(yard.height());
+		System.out.println(yard.width());
+		System.out.println(yard.getPos(5, 5));
+		yard.mow(5, 5);
+		yard.getPos(5, 5);
+		yard.showYard();
+	}
+}
---
+++ MowLawner/org/derryfield/Yard.java
@@ -0,0 +1,47 @@
+class Yard {
+	private char[][] yard;
+	public Yard(int i, int j) {
+		yard = new char[i + 2][j + 2];
+		fillYard();
+	} Yard() {yard = new char[12][12]; }
+
+	private char[][] fillYard() {
+		for (int i = 0; i < yard.length; i++) {
+			for (int j = 0; j < yard[i].length; j++) {
+				if (i == 0 || i == yard.length - 1 || j == 0 || j == yard[i].length - 1) {
+					yard[i][j] = 'R';
+				} else {
+					yard[i][j] = '+';
+				}
+			}
+		}
+		return yard;
+	}
+
+	public void showYard() {
+		for (int i = 0; i < yard.length; i++) {
+			for (int j = 0; j < yard[i].length; j++) {
+				System.out.print(yard[i][j]);
+			}
+			System.out.println();
+		}
+	}
+
+	public int height() {
+		return yard.length - 2;
+	}
+
+	public int width() {
+		return yard[0].length - 2;
+	}
+
+	public void mow(int x, int y) {
+		if (yard[x][y] != 'R') {
+			yard[x][y] = ' ';
+		}
+	}
+
+	public char getPos(int x, int y) {
+		return yard[x][y];
+	}
+}
